fe12213c3a714ab44d2bdf9141d42bb38136d177
UtopicSoft <tc@n-ix.com>
2019-01-14 12:16:27 +0200

    Create test2.pp

---
+++ manifests/test2.pp
@@ -0,0 +1,5 @@
+#
+#+ @name: test/resources/with2 items
+#+ @type: any
+#+ @desc: Installs and maintains Java installation under `/usr/lib/java` directory. Will test it again
+#+        reinstall it if removed.
